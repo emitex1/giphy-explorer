a245c6dccfaaecaf244d4b0246805defae8dac78
EmIT <emad.armoun@gmail.com>
2021-12-03 16:51:02 +0330

    fix: remove ts warning about tw

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from 'twin.macro';
 import 'styled-components/macro';
 import Header from './components/Header';
