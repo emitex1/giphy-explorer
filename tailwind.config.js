64b87cdf96438a3eaca35b062e0afe749206cf65
EmIT <emad.armoun@gmail.com>
2021-12-03 21:27:46 +0330

    style: define box shadow styles

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+      },
+      boxShadow: {
+        purple: '0 0 10px 5px #730096',
+        'purple-light': '0 0 15px 10px #8900a8'
+      }
+    },
+  },
+  plugins: [],
+}
