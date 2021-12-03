216f565b935b2fd572e40d50ae3a74d1d30225c0
EmIT <emad.armoun@gmail.com>
2021-12-03 19:34:03 +0330

    feat: make visible the title after a while

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,32 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from 'twin.macro';
 import 'styled-components/macro';
 import giphyMp4 from "../assets/images/giphy-color-shooting.mp4";
 
 const Header = () => {
+  const [isVisible, setIsVisible] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
 
   useEffect(() => {
     setTimeout(() => {
-      console.log(videoRef.current);
       videoRef.current && videoRef.current.play();
-    }, 4000);
+      setIsVisible(true);
+    }, 2000);
   }, []);
 
   return (
-    <div>
-      <video ref={videoRef} preload="true" width="400">
+    <div tw="flex justify-center text-center items-center">
+      <video ref={videoRef} preload="true" width="200">
         <source src={giphyMp4} type="video/mp4"></source>
-        Your browser does not support HTML5 video. Please update your browser.
+        GIPHY Logo
       </video>
+
+      <div css={[
+        tw`text-white text-4xl bg-black transition-opacity duration-1000 opacity-5`,
+        isVisible && tw`opacity-100`,
+        "transition-duration: 6000ms;"
+      ]}>GIPHY Explorer</div>
     </div>
   )
 }
