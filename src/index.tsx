b5e2efe615c9656e27ad7b8eeeb3c2f937f91bf9
EmIT <emad.armoun@gmail.com>
2021-12-03 13:20:20 +0330

    feat: add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import GlobalStyles from './GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <GlobalStyles />
+      <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
