29b2eea8fd81ab5dcc3445df9b11e63b67724fcf
EmIT <emad.armoun@gmail.com>
2021-12-03 19:46:41 +0330

    feat: add empty search box

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import tw from 'twin.macro';
 import 'styled-components/macro';
 import Header from './components/Header/Header';
+import SearchBox from './components/SearchBox/SearchBox';
 
 function App() {
   return (
     <div tw="container ">
       <Header />
+      <SearchBox />
     </div>
   );
 }
---
+++ src/components/SearchBox/SearchBox.tsx
@@ -0,0 +1,13 @@
+import SearchInput from "./SearchInput";
+import SearchResult from "./SearchResult";
+
+const SearchBox = () => {
+  return (
+    <div>
+      <SearchInput />
+      <SearchResult />
+    </div>
+  )
+}
+
+export default SearchBox;
