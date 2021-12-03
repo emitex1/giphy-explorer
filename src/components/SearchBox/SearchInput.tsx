39d428f8250ce0c0160a43d37409aa2a4ec51334
EmIT <emad.armoun@gmail.com>
2021-12-03 22:55:51 +0330

    feat: set state of input box

--- src/components/SearchBox/SearchInput.tsx
+++ src/components/SearchBox/SearchInput.tsx
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import tw from 'twin.macro';
 import 'styled-components/macro';
 
 const SearchInput = () => {
+  const [keyword, setKeyword] = useState('');
   return (
     <div tw="w-8/12 mx-auto my-5 text-center">
-      <input type="text" tw="transition-all border-none rounded-md shadow-purple focus:shadow-purple-light bg-gray-300 focus:bg-white text-sm text-purple-600 px-4 py-2 mx-auto w-3/12 focus:w-5/12" placeholder="Enter your keyword here to search ..." />
+      <input
+        type="text"
+        tw="transition-all border-none rounded-md shadow-purple focus:shadow-purple-light bg-gray-300 focus:bg-white text-sm text-purple-600 px-4 py-2 mx-auto w-3/12 focus:w-5/12"
+        placeholder="Enter your keyword here to search ..."
+        value={keyword}
+        onChange={e => setKeyword(e.target.value)}
+      />
     </div>
   )
 }
