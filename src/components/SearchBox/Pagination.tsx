72a67639a7917c756ca57fcf74a1ca626657c33b
EmIT <emad.armoun@gmail.com>
2021-12-04 09:47:01 +0330

    feat: add first & last page buttons

--- src/components/SearchBox/Pagination.tsx
+++ src/components/SearchBox/Pagination.tsx
@@ -11,6 +11,10 @@ interface PaginationProps {
 
 const Pagination = ({totalCount, pageSize, offset, setOffset}: PaginationProps) => {
 
+  const handleFirstClick = () => {
+    setOffset(0);
+  }
+
   const handlePrevClick = () => {
     if(offset === 0)
       return;
@@ -18,13 +22,17 @@ const Pagination = ({totalCount, pageSize, offset, setOffset}: PaginationProps) 
     setOffset(Math.max(0, offset - pageSize));
   }
 
-  const pageNumber = Math.ceil(totalCount / pageSize);
+  const pagesCount = Math.ceil(totalCount / pageSize);
   const currentPage = offset / pageSize;
 
   const handleNextClick = () => {
-    if(currentPage + 1 === pageNumber)
+    if(currentPage + 1 === pagesCount)
       return;
     setOffset(offset + pageSize);
+  }
+
+  const handleLastClick = () => {
+    setOffset( Math.round( (pagesCount - 1) * pageSize) );
   }
 
   const classes = {
@@ -34,19 +42,33 @@ const Pagination = ({totalCount, pageSize, offset, setOffset}: PaginationProps) 
 
   return (
     <div tw="flex justify-center items-center gap-10 mb-10">
+
+      <div css={[classes.btn, offset === 0 && classes.disabledCursor]} onClick={handleFirstClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+        </svg>
+      </div>
+
       <div css={[classes.btn, offset === 0 && classes.disabledCursor]} onClick={handlePrevClick}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
         </svg>
       </div>
 
-      {currentPage+1} / {pageNumber}
+      {currentPage+1} / {pagesCount}
 
-      <div css={[classes.btn, (currentPage+1 === pageNumber) && classes.disabledCursor]} onClick={handleNextClick}>
+      <div css={[classes.btn, (currentPage+1 === pagesCount) && classes.disabledCursor]} onClick={handleNextClick}>
         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
         </svg>
       </div>
+
+      <div css={[classes.btn, (currentPage+1 === pagesCount) && classes.disabledCursor]} onClick={handleLastClick}>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+          <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 5l7 7-7 7M5 5l7 7-7 7" />
+        </svg>
+      </div>
+
     </div>
   )
 }
