2f7ec9a3c2d88d416cbe3ded4ee5d7cb31faafbc
EmIT <emad.armoun@gmail.com>
2021-12-04 07:31:23 +0330

    feat: develop a simple thumbnail list

--- src/components/SearchBox/SearchResult.tsx
+++ src/components/SearchBox/SearchResult.tsx
@@ -1,12 +1,57 @@
+import { useEffect, useState } from 'react';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import tw from 'twin.macro';
+import 'styled-components/macro';
 import {useAppSelector} from '../../store/hooks';
+
+interface gifProps {
+  title: string;
+  thumbnail: string;
+  original: string;
+}
 
 const SearchResult = () => {
   const searchKeyword = useAppSelector( s => s.giphyReducer.searchKeyword );
+  const [gifs, setGifs] = useState<gifProps[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  console.log();
+
+  useEffect(() => {
+    if(process.env.REACT_APP_GIPHY_API_KEY) {
+      setIsLoading(true);
+      fetch("http://api.giphy.com/v1/gifs/search?api_key=" + process.env.REACT_APP_GIPHY_API_KEY + "&q=" + searchKeyword)
+      .then(response => response.json())
+      .then(data => {
+        const gifsResult: gifProps[] = data && data.data && data.data.map( (d: any) => ({
+          title: d.title,
+          thumbnail: d.images.fixed_width_small_still.url,
+          original: d.images.original.url
+        }));
+        setGifs(gifsResult);
+        setIsLoading(false);
+      });
+    }
+    else {
+      console.log("Please provide a valid API key for Giphy");
+    }
+  }, [searchKeyword])
 
   return (
-    <div>
-      SearchResult :
-      { searchKeyword }
+    <div tw="w-8/12 mx-auto my-5 text-center">
+
+      {isLoading && (
+        <span>Loading, Please Wait ...</span>
+      )}
+
+      <div tw="flex flex-wrap gap-6">
+        {!isLoading && gifs.map( g => (
+          <div tw="flex items-center flex-col w-32 cursor-pointer">
+            <img src={g.thumbnail} alt={g.title} tw="transition-all w-24 hover:w-28" />
+            <span tw="text-gray-300 overflow-auto">{g.title}</span>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
