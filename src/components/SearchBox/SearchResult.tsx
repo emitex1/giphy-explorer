cbed8d3f8313fa6e6c6ff0dae60a7c30b590908d
EmIT <emad.armoun@gmail.com>
2021-12-04 13:53:40 +0330

    style: improve ui & more info & use modal

--- src/components/SearchBox/SearchResult.tsx
+++ src/components/SearchBox/SearchResult.tsx
@@ -4,11 +4,15 @@ import tw from 'twin.macro';
 import 'styled-components/macro';
 import {useAppSelector} from '../../store/hooks';
 import Pagination from './Pagination';
+import Modal from '../Modal/Modal';
 
 interface gifProps {
   title: string;
   thumbnail: string;
   original: string;
+  downsized: string;
+  width: string;
+  height: string;
 }
 
 const SearchResult = () => {
@@ -19,6 +23,8 @@ const SearchResult = () => {
   const [totalCount, setTotalCount] = useState(0);
   const PAGE_SIZE = 12;
   const MAX_API_RESULT = 5000; // maximum result number of Giphy API
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const [currentGif, setCurrentGif] = useState<gifProps>();
 
   useEffect(() => {
     setOffset(0);
@@ -33,7 +39,10 @@ const SearchResult = () => {
         const gifsResult: gifProps[] = data && data.data && data.data.map( (d: any) => ({
           title: d.title,
           thumbnail: d.images.fixed_width_small_still.url,
-          original: d.images.original.url
+          original: d.images.original.url,
+          downsized: d.images.downsized_small.mp4,
+          width: d.images.original.width,
+          height: d.images.original.height
         }));
         setGifs(gifsResult);
         setTotalCount(Math.min(MAX_API_RESULT, data.pagination.total_count));
@@ -46,6 +55,11 @@ const SearchResult = () => {
   }, [searchKeyword, offset]);
 
   const emptyCells = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
+
+  const styles = {
+    mediaBox: tw`bg-gray-200 border-2 border-gray-400 mt-5`,
+    renditionBox: tw`flex-1 justify-center items-center text-center mb-4 text-sm`
+  }
 
   return (
     <>
@@ -61,7 +75,10 @@ const SearchResult = () => {
 
         <div tw="flex flex-wrap justify-center gap-6">
           {!isLoading && gifs.map( g => (
-            <div tw="flex items-center flex-col w-32 cursor-pointer">
+            <div tw="flex items-center flex-col w-32 cursor-pointer" onClick={ (e) => {
+              setCurrentGif(g);
+              setIsOpen(true);
+            }}>
               <img src={g.thumbnail} alt={g.title} tw="transition-all w-24 hover:w-28" />
               <span tw="text-gray-300 overflow-auto">{g.title}</span>
             </div>
@@ -74,6 +91,40 @@ const SearchResult = () => {
           <Pagination totalCount={totalCount} pageSize={PAGE_SIZE} offset={offset} setOffset={setOffset} />
         )
       }
+
+      <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
+        {currentGif && (
+          <div tw="flex flex-col items-center">
+
+            <div tw="text-center">
+              <span tw="text-purple-dark font-bold text-2xl">{currentGif.title}</span>
+
+              <img
+                src={currentGif.original} alt=""
+                width={currentGif.width} height={currentGif.height}
+                css={styles.mediaBox}
+              />
+            </div>
+
+            <div tw="flex h-1/6 mt-4 gap-4">
+
+              <div css={styles.renditionBox}>
+                <video preload="true" css={[styles.mediaBox, tw`h-full`]} autoPlay={true} loop={true}>
+                  <source src={currentGif.downsized} type="video/mp4"></source>
+                </video>
+                Down Sized Video
+              </div>
+
+              <div css={styles.renditionBox}>
+                <img src={currentGif.thumbnail} alt="" css={[styles.mediaBox, tw`h-full`]} />
+                Static Image
+              </div>
+
+            </div>
+
+          </div>
+        )}
+      </Modal>
     </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
+        'purple-dark': 'rgb(85, 0, 102)'
       },
       boxShadow: {
         purple: '0 0 10px 5px #730096',
         'purple-light': '0 0 15px 10px #8900a8'
+      },
+      minWidth: {
+        '1/4': '25%',
+        '1/3': '33.333%',
+        '1/2': '50%',
+      },
+      minHeight: {
+        '300': '300px'
       }
     },
   },
